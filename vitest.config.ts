4720e1a13c62d3c78b27486e7cc6145ea1644ac3
Pett, David <David.Pett@marriott-sp.com>
2025-11-03 16:04:53 -0600

    ci: install chromium-headless

--- vitest.config.ts
+++ vitest.config.ts
@@ -31,6 +31,7 @@ export default defineConfig({
           browser: {
             enabled: true,
             provider: 'playwright',
+            headless: true,
             instances: [{ browser: 'chromium' }],
           },
         },
